d92f0988d9d0cae38070c284a472a965ed4349a3
abzara20 <azaragozapalos@gmail.com>
2021-10-04 00:50:54 -0400

    create a project trying firebase

---
+++ first-firebase/public/app/app.js
@@ -0,0 +1,61 @@
+// this gonna b our database
+var _db;
+
+function initFirebase() {
+  firebase
+    .auth()
+    .signInAnonymously()
+    .then(() => {
+      // Signed in..
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      _db = [];
+    });
+  // associated _db with our firebase firestore
+  _db = firebase.firestore();
+}
+
+function initListeners() {
+  $(".submitBtn").click(function (e) {
+    let fn = $("#fName").val();
+    // shove the name into an object
+    let fnObj = { fName: fn };
+    // create a collection, or grab it if it exists. collection is like a named array of objects
+    _db
+      .collection("Names")
+      .add(fnObj)
+      .then(
+        function (doc) {
+          console.log("added doc " + doc.id);
+        },
+        function (error) {
+          console.log("error ", error);
+        }
+      );
+
+    $("#fName").val("");
+  });
+
+  $(".getBtn").click(function (e) {
+    _db
+      .collection("Names")
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          console.log(doc.data().fName);
+        });
+      });
+  });
+}
+
+$("document").ready(function () {
+  try {
+    let app = firebase.app();
+    initFirebase();
+    initListeners();
+  } catch {
+    console.error(e);
+  }
+});
